a2ef543b315294ed65a2d26f827b3688aba85cce
muji <freeformsystems@gmail.com>
2016-03-21 11:14:47 +0800

    Update readme build order.

--- mkdoc.js
+++ mkdoc.js
@@ -4,9 +4,9 @@ var mk = require('mktask');
 function readme(cb) {
   mk.doc('doc/readme.md')         // read markdown source document
     .pipe(mk.pi())                // parse processing instructions
-    .pipe(mk.msg())               // append generator message
     .pipe(mk.ref())               // include link references
     .pipe(mk.abs())               // make links absolute
+    .pipe(mk.msg())               // append generator message
     .pipe(mk.out())               // convert abstract syntax tree to markdown
     .pipe(mk.dest('README.md'))   // write the result to a file
     .on('finish', cb);            // proceed to next task
